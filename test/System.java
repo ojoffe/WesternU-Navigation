efaf74f44a7e7ff087ed34003b9980922846bd58
ojoffe2 <ojoffe2@uwo.ca>
2023-03-12 17:16:44 -0400

    Made edits to System class.

--- test/System.java
+++ test/System.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author orenj
  */
 
-import java.util*;
+import java.util.LinkedList;
 
 public class System {
     private int developerKey = 1;
-    private LinkedList<int> users;
+    private LinkedList<User> users;
+
+    public System() {
+        this.users = new LinkedList<>();
+    }
+
+    public User addUser(String systemID, String password) {
+        User newUser = new User(systemID, password);
+        this.users.add(newUser);
+        return newUser;
+    }
+
+    public User getUser(int index) {
+        return this.users.get(index);
+    }
+
+    public String getWeather() {
+        // TODO: Implement weather API logic here
+        return "Sunny";
+    }
 }
+
