20dcc674f895d6bae54c6a09403b814432b09f5c
ojoffe2 <ojoffe2@uwo.ca>
2023-03-12 18:37:13 -0400

    Made java project.

---
+++ WesternUNav/src/main/java/Mavenproject1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author orenj
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
