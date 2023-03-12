d09395761f57bf33ead6713157aa5589b3fda7d9
ojoffe2 <ojoffe2@uwo.ca>
2023-03-12 16:37:27 -0400

    Made System class and src folder

---
+++ test/System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author orenj
+ */
+
+import java.util*;
+
+public class System {
+    private int developerKey = 1;
+    private LinkedList<int> users;
+}
