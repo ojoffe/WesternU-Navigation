72118d12f9b807942e9d327dcd404f2197de16f4
ojoffe2 <ojoffe2@uwo.ca>
2023-03-15 18:11:02 -0400

    Made System class. Deleted MavenProject1.java

---
+++ WesternUNav/src/main/java/System.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author orenj
+ */
+
+import java.util.LinkedList;
+
+public class System {
+    private int developerKey = 1;
+    private LinkedList<User> users;
+
+    public System() {
+        this.users = new LinkedList<>();
+    }
+
+    public User addUser(String systemID, String password) {
+        User newUser = new User(systemID, password);
+        this.users.add(newUser);
+        return newUser;
+    }
+
+    public User getUser(int index) {
+        return this.users.get(index);
+    }
+
+    public String getWeather() {
+        // TODO: Implement weather API logic here
+        return "Sunny";
+    }
+}
