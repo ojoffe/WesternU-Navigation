ebd2f265e01202792fa7b60957edee5431f12bae
apogrin <apogrin@uwo.ca>
2023-03-25 18:31:21 -0400

    User class edit 1

--- WesternUNav/src/main/java/Middlesex1.java
+++ WesternUNav/src/main/java/Middlesex1.java
@@ -16,7 +16,7 @@ public class Middlesex1 extends javax.swing.JFrame {
     public Middlesex1() {
         initComponents();
     }
-
+//
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
