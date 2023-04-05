0dda64c4ea157977bca907ee059ae6459096a8f4
apogrin <apogrin@uwo.ca>
2023-04-05 18:58:38 -0400

    Javadoc

--- WesternUNav/src/main/java/Developer.java
+++ WesternUNav/src/main/java/Developer.java
@@ -6,8 +6,8 @@ public class Developer extends User {
     
     /**
      * This is the constructor for the developer which calls the user as a super
-     * @param systemID
-     * @param password 
+     * @param systemID represents the usernames
+     * @param password  represents the user's password
      */
     public Developer(String systemID, String password) {
         super(systemID, password);
@@ -15,11 +15,11 @@ public class Developer extends User {
     
     /**
      * This method adds a poi as a developer 
-     * @param layer
-     * @param id
-     * @param roomNum
-     * @param name
-     * @param coordinate
+     * @param layer represents a layer on the map
+     * @param id represents the poi id
+     * @param roomNum represents the room number
+     * @param name represents the poi name
+     * @param coordinate represents the coordinates on the map
      * @return 
      */
     public POI addPOI(String layer, int id, int roomNum, String name, int[] coordinate) {
