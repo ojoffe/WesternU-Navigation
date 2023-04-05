b7b6defb8c027fed1616357916b2fb7d0fbc23bb
lperri3 <lperri3@uwo.ca>
2023-04-05 15:58:40 -0400

    Fixed frame duplicating

--- WesternUNav/src/main/java/Talbot1.java
+++ WesternUNav/src/main/java/Talbot1.java
@@ -992,7 +992,6 @@ public class Talbot1 extends javax.swing.JFrame {
                     }
                     JOptionPane.showMessageDialog(null, "POI deleted successfully", "POI Deleted", JOptionPane.INFORMATION_MESSAGE);
                     hideFrame();
-                    new Talbot1(ll).setVisible(true);
                 } else {
                     JOptionPane.showMessageDialog(null, "Unable to find POI in JSON file", "Error", JOptionPane.ERROR_MESSAGE);
                 }
