d8715057869bc8861d64009931f90daef1ae4953
apogrin <apogrin@uwo.ca>
2023-04-05 18:11:18 -0400

    User class edit 1

--- WesternUNav/src/main/java/Layer.java
+++ WesternUNav/src/main/java/Layer.java
@@ -1,32 +1,54 @@
 /**
- *
+ * This class represents a layer in our map
  * @author Aaron
  */
 import java.util.LinkedList;
 
 
 public class Layer {
+    /** This String represents the layer's description*/
     private String description;
+    /** This linked list represents the list of POIs*/
     private LinkedList<POI> POIs;
 
-
+    /**
+     * This is the constructor for the layer
+     * @param description
+     * @param POIs 
+     */
     public Layer(String description, LinkedList<POI> POIs) {
         this.description = description;
         this.POIs = POIs;
     }
     
+    /**
+     * this method gets the description
+     * @return 
+     */
     public String getDescription(){
             return description;
     }
     
+    /**
+     * This method returns the POIs
+     * @return 
+     */
     public LinkedList<POI> getPOIs() {
         return POIs;
     }
     
+    /** 
+     * This method adds a poi 
+     * @param POI 
+     */
     public void addPOI(POI POI) {
         this.POIs.add(POI);
     }
     
+    /**
+     * This method deletes a poi
+     * @param POI 
+     */
     public void deletePOI(POI POI) {
         this.POIs.remove(POI);
     }
