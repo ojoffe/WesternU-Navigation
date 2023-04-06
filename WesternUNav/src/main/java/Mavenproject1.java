ba788057481f5f3cb232b02bcefb651f7408e87b
lperri3 <lperri3@uwo.ca>
2023-04-06 17:24:29 -0400

    Fixed Javadoc

--- WesternUNav/src/main/java/Mavenproject1.java
+++ WesternUNav/src/main/java/Mavenproject1.java
@@ -1,10 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- *
+ * Test
  * @author orenj
  */
 public class Mavenproject1 {
-
+    /**
+     * test
+     * @param args command line argument
+     */
     public static void main(String[] args) {
         CreateAccountFrame menu = new CreateAccountFrame();
         menu.setVisible(true);
